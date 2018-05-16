3a326ab420bf1940d437c262ae4205655ef06ff0
turchak <turchaksv@gmail.com>
2018-05-16 21:28:47 +0300

    add eslint

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import './Header.css';
+import React, {Component} from 'react';
+import Logo from '../../img/logo.png';
+
+export class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <div className="container header__container">
+                    <img src={Logo} alt="Logo" className="logo"/>
+                    <form className="search">
+                        <input className="search__input" type="text" name="name"/>
+                        <input className="search__button" type="submit" value="Search"/>
+                    </form>
+                </div>
+            </header>
+        )
+    }
+}
